9c4661b473fcc8b84fe0e7ae7b60ee9f26dded83
pmn0071 <pmn0071@autuni.ac.nz>
2024-09-18 11:54:16 +1200

    Implemented basic firebase login functionality

--- speed-frontend/src/app/page.tsx
+++ speed-frontend/src/app/page.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import { firebaseHandler } from "../controller/login";
+
+firebaseHandler.init();
 
 export default function Home() {
   return (
---
+++ speed-frontend/src/controller/login.ts
@@ -0,0 +1,35 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signOut, signInWithEmailAndPassword } from 'firebase/auth';
+import { firebaseConfig } from '../../secrets.firebase.js';
+
+export const firebaseHandler = {
+  async init() {
+    const firebase = await import('firebase/app');
+    await import('firebase/auth');
+    await firebase.initializeApp(firebaseConfig);
+    }
+};
+
+export const login = async (email: string, password: string) => {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error('Error logging in:', error);
+    throw error;
+  }
+};
+
+export const logout = async () => {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  try {
+    await signOut(auth);
+    console.log('User signed out successfully');
+  } catch (error) {
+    console.error('Error signing out:', error);
+    throw error;
+  }
+};
