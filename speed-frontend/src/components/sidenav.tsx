9c0876bb9d3459ee19ede79f98502ac55d939041
Joshua-Frost <gky3846@autuni.ac.nz>
2024-09-19 10:19:56 +1200

    Added initial sidenav component

---
+++ speed-frontend/src/components/sidenav.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useState } from "react";
+import {
+  Box,
+  Drawer,
+  Button,
+  List,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+
+//import static user object
+
+export default function Sidenav() {
+  const [open, setOpen] = useState(true);
+
+  const toggleDrawer = (open: boolean) => () => {
+    setOpen(open);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText primary="Moderation" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText primary="Analysis" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText primary="Logout" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+  return (
+    <div>
+      <Drawer variant="permanent" open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
