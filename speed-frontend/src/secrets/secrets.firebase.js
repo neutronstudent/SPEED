723cee8ded6406e2ba4ad2d4969d740fc3adf91c
Joshua-Frost <gky3846@autuni.ac.nz>
2024-09-23 21:04:26 +1200

    re-added secrets

---
+++ speed-frontend/src/secrets/secrets.firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_KEY,
+    authDomain: "speed-app-e3cc6.firebaseapp.com",
+    projectId: "speed-app-e3cc6",
+    storageBucket: "speed-app-e3cc6.appspot.com",
+    messagingSenderId: "221620017436",
+    appId: "1:221620017436:web:d0224c9d0323cf2c4e42d9"
+};
+
+export default firebaseConfig; 
