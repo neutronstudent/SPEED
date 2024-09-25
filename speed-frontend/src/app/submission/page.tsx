615e0ad73e05266c3d00733bff22144b19e7ff3c
Joshua-Frost <gky3846@autuni.ac.nz>
2024-09-25 16:46:11 +1200

    Added submission page route

---
+++ speed-frontend/src/app/submission/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import SubmissionForm from "@/components/SubmissionForm";
+
+const SubmissionPage: React.FC = () => {
+  return <SubmissionForm />;
+};
+
+export default SubmissionPage;
