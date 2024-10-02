a4c5a51e7621039fbcaefbe671949e141761a1cf
neutrongreen <ctp1858@autuni.ac.nz>
2024-10-02 22:28:57 +1300

    Try again

--- speed-backend/src/main.ts
+++ speed-backend/src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);  
-  const port = process.env.PORT || 8082;
-  app.use(cors());
-  await app.listen(port, () => console.log(`Server running on port ${port}`));  
+  app.enableCors();
+  //app.use(cors());
+  await app.listen(3000);  
 }
 
 bootstrap();
