3670bca1a90746290510251b1d855519ee454d96
neutrongreen <joshua@green.geek.nz>
2024-10-14 17:38:57 +1300

    force dynamic for login page

--- speed-frontend/src/app/login/page.tsx
+++ speed-frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Login from "@/components/Login";
-
+export const dynamic = 'force-dynamic'
 // Displays Login component on the page
 const LoginPage: React.FC = () => {
   return (
