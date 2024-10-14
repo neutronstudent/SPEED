8c4efe0855135603afe4a54b9193bc8c3f394acc
neutrongreen <joshua@green.geek.nz>
2024-10-14 17:44:00 +1300

    revet changes

--- speed-frontend/src/app/login/page.tsx
+++ speed-frontend/src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Login from "@/components/Login";
 //force dynamic rendering to see if it fixes issue
-export const dynamic = 'force-dynamic'
+//export const dynamic = 'force-dynamic'
 // Displays Login component on the page
 const LoginPage: React.FC = () => {
   return (
